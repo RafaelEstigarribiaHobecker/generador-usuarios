d82a45d44f9a2bf197d02ef4afc847496307f956
Rafael Estigarribia <aldorfaelhobecker04@fpuna.edu.py>
2026-02-06 17:17:10 -0300

    feat: implementar motor de filtrado avanzado con operadores

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Simulamos una base de datos de la Facultad
 
-const alumnos = [
-    {id: 1, nombre: "Rafael", carrera: "Informática", semestre: 3, promedio: 4.5},
+const alumnos = [{id: 1, nombre: "Rafael", carrera: "Informática", semestre: 3, promedio: 4.5},
     {id: 2, nombre: "Ana", carrera: "Electrónica", semestre: 3, promedio: 3.8},
     {id: 3, nombre: "Marcos", carrera: "Informática", semestre: 8, promedio: 4.2},
     {id: 4, nombre: "Luz", carrera: "Civil", semestre: 3, promedio: 4.9}
-
 ];
 
 
@@ -15,13 +13,34 @@ const buscarAlumnos = (lista, propiedad, valor) => {
 
 }
 
+const filtrarAvanzado = (lista, propiedad, operador, valor) => {
+    return lista.filter(alumno => {
+        if (operador === ">") return alumno[propiedad] > valor;
+        if (operador === "<") return alumno[propiedad] < valor;
+        if (operador === "===") return alumno[propiedad] === valor;
+        return false;
+    });
+};
+
 
 // Buscar por carrera
 
-console.log("--Alumnos de Informática ---");
+console.log("--- Alumnos de Informática ---");
 console.table(buscarAlumnos(alumnos, "carrera", "Informática"));
 
 //Buscar por semestre
 
-console.log("---Alumnos del 3er Semestre ---");
+console.log("--- Alumnos del 3er Semestre ---");
 console.table(buscarAlumnos(alumnos, "semestre", 3));
+
+
+//Promedio Mayor a 4.0
+console.log("--- Alumnos con promedio mayor a 4.0 ---");
+console.table(filtrarAvanzado(alumnos, "promedio", ">", 4.0));
+
+
+//Alumnos de Semestres Iniciales(menor al 4to)
+
+console.log("--- Alumnos de semestres iniciales (menor al 4to) ---");
+console.table(filtrarAvanzado(alumnos, "semestre", "<", 4));
+
