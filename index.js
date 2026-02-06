b65c5dc121c8515052d43ff887037496c16f6e62
Rafael Estigarribia <aldorfaelhobecker04@fpuna.edu.py>
2026-02-05 22:12:00 -0300

    feat: lógica de filtrado de alumnos corregida

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 // Simulamos una base de datos de la Facultad
 
-const alumnos=[
-    {id:1,nombre:"Rafael",carrera:"Informática",semestre:5},
-    {id:2,nombre:"Ana",carrera:"Electrónica",semestre:3},
-    {id:3,nombre:"Marcos",carrera:"Informática",semestre:8}
+const alumnos = [
+    {id: 1, nombre: "Rafael", carrera: "Informática", semestre: 5},
+    {id: 2, nombre: "Ana", carrera: "Electrónica", semestre: 3},
+    {id: 3, nombre: "Marcos", carrera: "Informática", semestre: 8}
 
 ];
 
 
-
-const filtrarPorCarrera = (lista,carrera) =>{
-    return lista.filter(alumno=>alumno.carrera===carrera);
+const filtrarPorCarrera = (lista, carrera) => {
+    return lista.filter(alumno => alumno.carrera === carrera);
 };
 
 console.log("--Alumnos de Informática ---");
-console.table(filtrarPorCarrera(alumnos,"Informática"));
+console.table(filtrarPorCarrera(alumnos, "Informática"));
